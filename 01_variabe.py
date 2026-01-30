1b7e26ba65b63493c4c5eb88f5400237ea565486
MercyAdebiyi <adebiyim008@gmail.com>
2026-01-30 11:30:36 +0000

    An intro to variable creation and assignment

---
+++ 01_variabe.py
@@ -0,0 +1,17 @@
+# variables are containers used to store data.
+
+# data types are many in python. Some are numbers(int/whole number and float/decimals), and strings(alphabets). other data types will be discusssed later.
+
+age=20
+husband_age="30.5"
+name="samson"
+# total_age= age+husband_age
+
+# inputs are always strings data types. So it must be converted before using a number (int/float)
+
+weight=int(input("enter your weight: "))
+height= float(input("enter your height"))
+calculate= age*weight*height
+print(calculate)
+
+# debugging is used to find errors in code and to understand the thinking process of the computer. This is accessed by using the deugger and setting a break point (a small red dot on the side)
