0796b40ed676558f2854709675815cd1f44ea021
MercyAdebiyi <adebiyim008@gmail.com>
2026-01-30 11:35:53 +0000

    template literal and string interpolation.
    Using f-string to concatenate.

---
+++ 02_template_literal.py
@@ -0,0 +1,11 @@
+# concatenation is a way of joining strings and variables together. concatenation is done for only strings data types
+
+age=209
+name="jidenna"
+surname="uche"
+
+# print(name + " " + surname)
+
+# template literal is used to print output with different data types
+
+print(f"my name is {name} {surname}. I am {age} years old.")
