480e540d6a48dadff1d1a26f090cdf5ab64740b4
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-09 01:13:02 +0000

    Add files via upload

---
+++ 03_simple_exercise.py
@@ -0,0 +1,8 @@
+exam=input("enter your exam paper name: ")
+first_name=input("enter your first name: ")
+surname=input("enter your surname: ")
+admin_num=input("enter your admin_num: ")
+age=int(input("enter your age: "))
+test_score=float(input("enter your test_score: "))
+
+print(f"Hello {first_name} {surname}! You are welcome to {exam} exam. Your admission number is {admin_num}. Your current age is {age} years old and you are qualified to write this exam. Your test score is {test_score}. Good luck.")
