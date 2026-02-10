6f473952179012b224bad9c72dbcbbfcfbf4ac58
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-10 00:45:22 +0000

    visitors attendance

---
+++ 04_my_exercise.py
@@ -0,0 +1,10 @@
+print("**********VISITOR ATTENDANCE**********")
+
+
+visitor_name=input("enter your name: ")
+time_of_visit=input("enter the time: ")
+time_of_exit=input("enter the time: ")
+whom_to_see=input("enter whom to see: ")
+purpose_of_visit=input("what is your purpose of visit: ")
+
+print(f"Hi! {visitor_name}. When did you arrive here {time_of_visit}. When are you leaving {time_of_exit}. whom are you here to see {whom_to_see}. what is your purpose of visit {purpose_of_visit}")
