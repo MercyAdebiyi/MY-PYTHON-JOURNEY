9812f27f68cd633fbe587efe5e46a0b32b0ad752
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-11 01:44:59 +0000

    Add files via upload
    Total sum, average and raise to power

---
+++ 05_arithemetic_operators.py
@@ -0,0 +1,32 @@
+# operators are mathematical tools used for calculations
+
+# + addition, - substraction, / division, * multiplication, ** raise to power, %% modulus
+
+cup_of_garri= 500
+rice= 1800
+beans= 1000
+spag= 750.32
+chicken= 1960
+tomato=300
+
+# Question 1: find the sum of all the items
+
+total_sum= cup_of_garri + rice + beans + spag + chicken + tomato
+print(f"The total cost is {total_sum}")
+
+# Question 2: find the average
+
+average= total_sum/6
+print(f"Average cost for the items is {average}")
+
+# question 3: find cost of buying the double of everything
+
+double_cost=total_sum**2
+print(f"the double of the total price is {double_cost}")
+
+
+# Question 4: find the remainder of dividing rice by tomato 
+
+remainder= rice % beans
+print(f"the remainder of dividing rice and tomato is {remainder}")
+
