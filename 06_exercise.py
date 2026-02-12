80dee550da9b4ed35a2447f72804540686826b9d
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-12 09:28:40 +0000

    Add files via upload

---
+++ 06_exercise.py
@@ -0,0 +1,7 @@
+double_number= float(input("Please enter digits to be doubled: "))
+double= double_number*2
+
+raised_number =float(input("Enter to raise to the power of 2"))
+raised= raised_number**2
+
+print(f"The doubled number {double} is not the same as the raised number {raised}")
