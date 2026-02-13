7a31053eb2a8003f00c6bd474e4f7a86d3df2623
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-13 14:09:29 +0000

    Add files via upload

---
+++ 07_bmi_exercise.py
@@ -0,0 +1,11 @@
+name=input("please enter your name: ")
+print(f"Hello {name}! Welcome to Yemi Diagnosis Health Clinic")
+
+
+weight= float(input("enter your weight in kg: "))
+height= float(input("enter your height in meter: "))
+
+bmi= weight / (height **2)
+print(f" Your bmi is {bmi}")
+
+print("Thank you for visiting us. See you again soon!")
