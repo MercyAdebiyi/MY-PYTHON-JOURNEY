7a0504563a0d5ea02ba89679674e315aafeaf013
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-17 17:06:08 +0000

    Add files via upload

---
+++ 09_exercise.py
@@ -0,0 +1,13 @@
+surname= input(" Enter your name in capital letters: ").upper().strip()
+# this converts the surname to capital letters
+
+firstname= input("Enter your firstname in small letters: ").lower().strip()
+
+print(f"Hi, {surname} {firstname}! Welcome to my page. ")
+
+
+# area= input("enter your area: ")
+# area= area.upper()
+# area=area.strip()
+
+# print (f"you live at {area}")
