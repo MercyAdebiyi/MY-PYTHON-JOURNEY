3e8ae76286216ff2d8ad6c043aaa94930329493c
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-24 22:04:48 +0000

    Add files via upload

---
+++ 016.grade_checker_exercise.py
@@ -0,0 +1,26 @@
+# top_and_bottom="**************************************"
+# mid="""
+# **                                 **
+# """
+# content= "            GRADE CHECKER"
+# print(top_and_bottom,mid, content, mid, top_and_bottom)
+
+
+# code proper 
+grade= float(input("Enter grade from 0-100: "))
+
+if grade>=70:
+    print("A")
+
+elif grade>=60:
+    print("B")
+
+elif grade>=50:
+    print ("C")
+
+elif grade>=40:
+    print("D")
+
+else:
+    print("F")
+
