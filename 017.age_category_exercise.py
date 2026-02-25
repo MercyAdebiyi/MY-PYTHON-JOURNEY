7cba3390cb191f102d438e14b4cd25b482ba4342
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-25 10:40:07 +0000

    Add files via upload

---
+++ 017.age_category_exercise.py
@@ -0,0 +1,13 @@
+age= float(input("Enter your age to know your category: "))
+
+if age>= 70:
+    print("You are a Senior Citizen")
+
+elif age >= 20:
+    print("You are an adult")
+
+elif age >= 13:
+    print("You are a Teenager")
+
+else:
+    print("You are a child.")
