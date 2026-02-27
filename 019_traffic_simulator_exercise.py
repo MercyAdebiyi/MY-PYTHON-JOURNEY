a8859e772a7bf34cf3bddb98024ded954a9f4cfd
MercyAdebiyi <adebiyim008@gmail.com>
2026-02-27 17:07:49 +0000

    Add files via upload

---
+++ 019_traffic_simulator_exercise.py
@@ -0,0 +1,10 @@
+colour= input("enter colour (red, yellow, or green): ").strip().lower()
+
+if colour=="red":
+    print("STOP")
+
+elif colour=="yellow":
+    print("READY")
+
+else:
+    print("GO")
