ee2b6ccd9b9abe0f07ba195aed91a65149156f7c
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-01 19:05:36 +0000

    Add files via upload

---
+++ 021_NUMBER_COMPARISON_EXERCISE.py
@@ -0,0 +1,11 @@
+first_number=float(input("enter number: "))
+second_number=float(input("enter number: "))
+
+if first_number>second_number:
+    print("the first number is greater")
+
+elif first_number<second_number:
+    print("the first number is smaller")
+
+else:
+    print("first number is equal to the second number")
