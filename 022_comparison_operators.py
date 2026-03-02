4af6e851ca0ddc1d0351f1658b294e0764e59380
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-02 18:08:30 +0000

    Add files via upload

---
+++ 022_comparison_operators.py
@@ -0,0 +1,34 @@
+# comparison operators are used to compare different items or variables.
+
+# 1. == equality 
+
+# 2. != not equal
+
+# 3. >= gretaer than or equal to 
+
+# 4. <= less than or equal to 
+
+# 5. less than <
+
+# 6. gretaer than >
+
+# age =13
+# user_input= int(input("Enter age: "))
+
+# if age==user_input:
+#     print("We are the same numbers")
+
+# elif age>user_input:
+#     print("number is too large")
+
+# else: 
+#     print("number too small")
+
+print(12>12)
+
+# comparison operators can also be used on strings
+
+pass1="banana"
+pass2="oranges"
+
+print(pass1>pass2)
