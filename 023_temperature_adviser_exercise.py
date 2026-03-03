da95ba5e349c3c59cda591b3d8276e4a0ed42b36
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-03 23:47:21 +0000

    Add files via upload

---
+++ 023_temperature_adviser_exercise.py
@@ -0,0 +1,10 @@
+temperature=float(input("enter your temperature: "))
+
+if temperature<=10:
+    print("it is freezing")
+
+elif temperature<=22:
+    print("wear your jacket")
+
+else:
+    print("it is very hot")
