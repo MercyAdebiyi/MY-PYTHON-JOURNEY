105fc456d0fabc4642b7c4bf2a4d536a5a907d01
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-04 22:02:06 +0000

    Add files via upload

---
+++ 024_day_type_checker.py
@@ -0,0 +1,27 @@
+day=input("enter your day (Monday, Tuesday, etc.)").strip().title()
+
+if day=="Monday":
+    print("monday is a weekday.")
+
+elif day=="Tuesday":
+    print("Tuesday is a weekday.")
+
+
+elif day=="Wednesday":
+    print("Wednesday is a weekday.")
+
+
+elif day=="Thursday":
+    print("Thursday is a weekday.")
+
+
+elif day=="Friday":
+    print("Friday is a weekday.")
+
+
+elif day=="Saturday":
+    print("Saturday is a weekend.")
+
+
+else:
+    print("Sunday is weekend")
