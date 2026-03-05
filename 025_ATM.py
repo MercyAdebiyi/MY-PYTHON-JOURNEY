891c5fdedb670cde0c9e4c1b9c5a85802c6bf29c
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-05 07:24:19 +0000

    Add files via upload

---
+++ 025_ATM.py
@@ -0,0 +1,10 @@
+choose=input("please enter an option (1 = check balance, 2 = withdraw, 3 = deposit): ").strip()
+
+if choose=="1":
+    print("balance checked")
+
+elif choose=="2":
+    print("withdrawal successful")
+
+else:
+    print("deposit completed")
