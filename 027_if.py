f19375884345fa29d5b3920b91a27388d6952bf6
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-07 18:32:06 +0000

    Add files via upload

---
+++ 027_if.py
@@ -0,0 +1,13 @@
+print("""  Welcome, everyone!
+We’re so happy to have all of you here today. Family is about love, laughter, and the special moments we share, and today is all about celebrating that togetherness. Thank you for coming to make this gathering even more meaningful. Let’s relax, enjoy the food, share stories, and create beautiful memories.
+      
+Welcome, and let the celebration begin! """)
+
+name=input("enter your surname: ").title().strip()
+
+if name == "Adebiyi":
+     print("Congratulation! you're welcome")
+
+else:
+     print("Sorry you're not welcome in this gathering")
+
