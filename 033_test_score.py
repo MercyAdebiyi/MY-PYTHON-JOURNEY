3c53f55dfe6c98b75e828feb6222aeb551d8f56a
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-13 08:49:44 +0000

    Add files via upload

---
+++ 033_test_score.py
@@ -0,0 +1,12 @@
+test_score=float(input("enter test score 0-100: "))
+
+if test_score>=50:
+    if test_score>=70:
+        print("Merit")
+    else:
+        print("Pass")
+
+else:
+    print("Fail")
+
+
