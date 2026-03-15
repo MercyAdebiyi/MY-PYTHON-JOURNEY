be06c0e864e03b5658fa281242024a2db3b5cd6b
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-15 15:51:50 +0000

    Add files via upload

---
+++ 035_ticket_pricing_exercise.py
@@ -0,0 +1,11 @@
+age=int(input("enter your age: "))
+
+if age <18:
+    print("Ticket cost is $5")
+
+else:
+    if age>=65:
+        print("Ticket cost is $6")
+    else:
+        print("Ticket cost is $10")
+
