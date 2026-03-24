2dbcc3f79d0daa589fb80260d236a588c7f97b7f
MercyAdebiyi <adebiyim008@gmail.com>
2026-03-24 22:14:42 +0000

    Add files via upload

---
+++ 044_tenary_operation.py
@@ -0,0 +1,10 @@
+# the tenary op[erator is the short form of an if else statement. This not aplicable to elif statements. 
+
+age=18
+
+# if age>=18:
+#     print("you are an adult")
+# else: 
+#     print("you are a child.")
+
+print("you are an adult") if age>=18 else print("you are an child.")
