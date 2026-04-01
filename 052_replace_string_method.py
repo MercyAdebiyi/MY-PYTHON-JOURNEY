92eb0755281f7b565cff1e554a1155c807150fed
MercyAdebiyi <adebiyim008@gmail.com>
2026-04-01 16:47:48 +0100

    Add files via upload

---
+++ 052_replace_string_method.py
@@ -0,0 +1,7 @@
+# EXERCISE 3
+name= input("enter a name in any capitalization ans i will reverse it.: ").swapcase()
+name2=input("enter wife name: ").upper()
+
+name3= name.replace(name, name2)
+
+print(name3)
