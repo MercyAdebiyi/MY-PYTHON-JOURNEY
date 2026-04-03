961d413c2c67d4afb53d88f7deeeb1a3e2c49fc9
MercyAdebiyi <adebiyim008@gmail.com>
2026-04-03 21:04:26 +0100

    Add files via upload

---
+++ 054_slicing_exercise.py
@@ -0,0 +1,15 @@
+# * Exercise 1 – Basic slicing*
+#   Given the string text = "CyberSecurity", write a slice that returns "Cyber" and another slice that returns "Security".
+
+print("\n**************EXERSICE 1*******************""\n")
+
+course="CyberSecurity"
+print(f"{course[0:5]}")
+print(f"{course[5:13]}")
+
+
+
+print("\n**************STRING REVERSE*******************""\n")
+print(course[::-1])
+
+
