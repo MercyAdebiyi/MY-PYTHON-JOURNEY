a9217bc3e9388282234665943462536758a0de4c
MercyAdebiyi <adebiyim008@gmail.com>
2026-04-07 11:53:48 +0100

    Add files via upload

---
+++ 058_SUMMARY.py
@@ -0,0 +1,45 @@
+# 1. DATA TYPES
+    # -Strings
+    # -numbers (int and float)
+    # -Boolean (True or False)
+
+
+
+# 2. OPERATORS
+    # -Arithemetic
+    # -comparison
+    # -Logical
+    # -assignment
+    # -membership
+    # -identity
+
+
+# 3. CONDITIONAL STATEMENTS
+    # -If statement 
+    # -elifs statements
+    # -nested ifs
+    # -tenary operators
+
+
+# 4. STRINGS DATA TYPE
+    # -String methods
+    # -string slicing
+
+
+
+# 5. STRINGS DATA TYPE
+    # -number methods
+    # -number functions
+
+
+# 6. input and output
+    # -fstring/string interpolation/ template literals
+    # -multiline print
+    # -multiple variable declaration
+    # -input manipulation
+    # -escape characters
+    # -type casting
+
+
+
+# 7. EXERCISES
