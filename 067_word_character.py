468b6a283cfafbb5bd6f1dae321edd8ecca9ff82
MercyAdebiyi <adebiyim008@gmail.com>
2026-04-18 11:59:34 +0100

    Add files via upload

---
+++ 067_word_character.py
@@ -0,0 +1,36 @@
+# Ask the user for a word and print each character on a new line using a `while` loop.
+
+word=input("enter a word: ").strip()
+
+i=0
+while i<len(word):
+    print(word[i])
+    i+=1
+
+
+# indexing
+# [i]---- this is an index 
+# [0], [1]
+# name="samson"--- name[i]=name[0, 1, 2, 3, 4, 5]
+
+# len(name)----6
+
+
+
+
+# while len(word):
+#     word=+1
+
+    # print(f"{word[0]}")
+    # print(f"{word[1]}")
+    # print(f"{word[2]}")
+    # print(f"{word[3]}")
+    # print(f"{word[4]}")
+    # print(f"{word[5]}")
+    # print(f"{word[6]}")
+    # print(f"{word[7]}")
+# else:
+
+
+
+# print(len(word))
