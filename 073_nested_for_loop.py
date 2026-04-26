fe215ab1f55060b27132a797e7b72d981c5a88db
MercyAdebiyi <adebiyim008@gmail.com>
2026-04-26 20:33:27 +0100

    Add files via upload

---
+++ 073_nested_for_loop.py
@@ -0,0 +1,16 @@
+name="Mercy Adebiyi"
+index="1234567890"
+
+# These are 2 separate for loops
+
+# for x in name:
+#     print(x)
+# for y in index:
+#     print(y)
+
+
+# This is a nested loop 
+
+for x in name:  
+    for y in index:
+        print(x, y)
